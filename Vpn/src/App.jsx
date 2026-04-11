44cb10fc0a2192e384325bed850c40ffe3bfac46
gunes <gunes@gmail.com>
2026-04-11 13:51:38 +0400

    gh-pages

---
+++ Vpn/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+  const eded1 = +prompt('eded 1 daxil edin:')
+  const eded2 = +prompt('eded 2 daxil edin:')
+ 
+  const cem = eded1+ eded2
+  return (
+    <>
+   <h1>Magsudova Gunesh</h1>
+   
+   <h1>{cem}</h1>
+ 
+    </>
+    
+  )
+}
+
+export default App
---
+++ gunes/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <h1>Saam react</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At, officia ut? Fugit vero, itaque cumque molestiae harum sapiente accusantium eius magni voluptates doloremque commodi, beatae impedit quasi libero delectus illo.</p>
+    </>
+  )
+}
+
+export default App
